a113cb7a79adde47a360b71aaf67621fe3b6e589
Ryan Bassit <Rbassit510@gmail.com>
2016-10-19 16:52:45 -0400

    Add validations to Databasefiles

--- code/models/stats.js
+++ code/models/stats.js
@@ -1,11 +1,46 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Stats = sequelize.define('Stats', {
-    username: DataTypes.STRING,
-    height: DataTypes.INTEGER,
-    weight: DataTypes.INTEGER,
-    gender: DataTypes.STRING,
-    homeGym: DataTypes.STRING
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate{
+        notEmpty: true,
+        isAlpha: true
+      }
+    },
+
+    height: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate{
+        notEmpty: true,
+        isNumeric: true
+      }
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate{
+        notEmpty: true,
+        isNumeric: true
+      }
+    },
+    gender: {
+      type: DataTypes.STRING,
+      allowNull:false,
+      validate{
+        notEmpty: true,
+        isAlpha: true
+      }
+    },
+    homeGym: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate{
+        notEmpty: true
+      }
+    }
   }, {
     classMethods: {
       associate: function(models) {
--- code/models/user.js
+++ code/models/user.js
@@ -1,11 +1,45 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate{
+        notEmpty: true,
+        isAlpha: true
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate{
+        notEmpty: true,
+        isAlpha: true
+      }
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate{
+        notEmpty: true
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate{
+        notEmpty: true,
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING
+      allowNull: false,
+      validate{
+        notEmpty: true,
+        isAlphanumeric: true
+      }
+    }
   }, {
     classMethods: {
       associate: function(models) {
