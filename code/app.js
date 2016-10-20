045049f4119e4dfbf02ad509b503b2bf42d20b39
Ryan Bassit <Rbassit510@gmail.com>
2016-10-20 16:01:38 -0400

    Add handlebars templating engine

--- code/app.js
+++ code/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
-//const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({
+	layoutsDir: './views/layouts',
+	defaultLayout: 'main',
+}));
+
+app.set('view engine', 'handlebars');
+app.set('views', '${__dirname}/views/');
 
 app.get('/', function (req, res) {
   res.send('The / home page');
 });
-
 
 // Load and mount the home controller
 const home = require('./controllers/home');
@@ -26,19 +33,6 @@ app.use('/profile', profile);
 // Load and mount the leaderboard controller
 const leaderboard = require('./controllers/leaderboard');
 app.use('/leaderboard', leaderboard);
-
-// // Load and mount the leaderboard controller
-// const articles = require('./controllers/articles');
-// app.use('/articles', articles);
-
-//app.engine('handlebars', exphbs({
-//	layoutsDir: './views/layouts',
-//	defaultLayout: 'main',
-//}));
-
-//app.set('view engine', 'handlebars');
-//app.set('views', '${__dirname}/views/');
-
 
 module.exports = app;
 app.listen(8000);
