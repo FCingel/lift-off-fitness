5e3785c39cc3fb5ef8a7cb137af38aba1f74184e
Ryan Bassit <Rbassit510@gmail.com>
2016-10-21 19:29:51 -0400

    Add validations to database

--- code/models/user.js
+++ code/models/user.js
@@ -1,11 +1,46 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        isAlphanumeric: true
+      }
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        isAlpha: true
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        isAlpha: true
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING
+     // allowNull: false,
+     // validate: {
+     //    notEmpty: true,
+     //    len: [6,100]
+     //  }
+    }
   }, {
     classMethods: {
       associate: function(models) {
