a873ff9bc87fd6ddac8c4491cb88cde6261e3d32
Edouard Michel <MathMinor@users.noreply.github.com>
2016-11-20 13:05:23 -0500

    Add leaderboard template and profile skeleton.

--- code/controllers/profile.js
+++ code/controllers/profile.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var router = express.Router();
 
 // middleware that is specific to this router
@@ -17,8 +17,34 @@ router.get('/', function(req, res) {
 // define the specific profile route
 // Note: 'slug' is how we refer to document titles in url's
 // For some history checkout: http://stackoverflow.com/a/4230937
-router.get('/:slug', function(req, res) {
-  res.send('This is profile: ' + req.params.slug);
+router.get('/profile', function(req,res){
+  res.send('profile/profile');
+};
+
+
+module.exports = router;*/
+var express = require('express');
+var router = express.Router();
+var models = require('../models');
+
+// middleware that is specific to this router
+// applies to all routes defined in this controller
+router.use(function timeLog(req, res, next) {
+  console.log('user Controller :: Time: ', Date.now());
+  next();
+});
+
+
+// define the root users route
+router.get('/', function(req, res) {
+  models.User.findAll({})
+    .then(function (users) {
+      if (users != null) {
+        res.render('profile/profile', {users: users});
+      } else {
+        res.send('users page not found');
+      }
+    });
 });
 
 module.exports = router;
