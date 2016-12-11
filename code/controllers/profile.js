9b63672b7b4328e8dfa57a5e3cb001cb3675ee93
Ryan Bassit <Rbassit510@gmail.com>
2016-12-11 00:29:31 -0500

    Update profile page and add images

--- code/controllers/profile.js
+++ code/controllers/profile.js
@@ -29,18 +29,18 @@ var models = require('../models');
 // middleware that is specific to this router
 // applies to all routes defined in this controller
 router.use(function timeLog(req, res, next) {
-  console.log('user Controller :: Time: ', Date.now());
+  console.log('profile Controller :: Time: ', Date.now());
   next();
 });
 
-// define the root profile route
+//define the root profile route
 router.get('/', function(req, res) {
   models.Stats.findAll({
     where: {
-      id: 1
+      id: 4
     }
   })
-    .then(function (stats) {
+  .then(function (stats) {
       if (stats != null) {
         res.render('profile/profile', {stats: stats});
       } else {
@@ -48,6 +48,21 @@ router.get('/', function(req, res) {
       }
     });
 });
+
+// router.get('/', function(req, res) {
+//   models.User.findAll({
+//     where: {
+//       id: 4
+//     }
+//   })
+//     .then(function (users) {
+//       if (users != null) {
+//         res.render('profile/profile', {users: users});
+//       } else {
+//         res.send('profile page not found');
+//       }
+//     });
+// });
 
 
 
@@ -57,23 +72,23 @@ router.get('/profile', function (req,res) {
 });
 
 
-router.put('/', function(req,res) {
-  console.log(req.body);
-  models.Stats.update({
-    bench_press: req.body.bench_press,
-    military_press: req.body.military_press,
-    squat: req.body.squat,
-    deadlift: req.body.deadlift,
-    height: req.body.height,
-    weight: req.body.weight
-  }).then(function (profile) {
-    //SHOULD UPDATE PROFILE 
-    res.redirect('/profile')
-  }).catch(function (e) {
-    res.render('profile', {errors: e.errors});
-    //res.json(e);
-  })
-});
+// router.put('/', function(req,res) {
+//   console.log(req.body);
+//   models.Stats.update({
+//     bench_press: req.body.bench_press,
+//     military_press: req.body.military_press,
+//     squat: req.body.squat,
+//     deadlift: req.body.deadlift,
+//     height: req.body.height,
+//     weight: req.body.weight
+//   }).then(function (profile) {
+//     //SHOULD UPDATE PROFILE 
+//     res.redirect('/profile')
+//   }).catch(function (e) {
+//     res.render('profile', {errors: e.errors});
+//     //res.json(e);
+//   })
+// });
 
 
 
